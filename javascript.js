acddc7c302d47706f64519a08a55042b85810cfe
aditya kumar <adityakumar1995rawat@gmail.com>
2021-11-09 12:38:48 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,22 @@
+function fun() {
+    let a, b, c;
+    // document.getElementById('p1').style.backgroundColor='black';
+    for (a = 0; a < 1000; a++) {
+        if (a % 2 == 0)
+            document.getElementById('p1').style.backgroundColor = 'white';
+        else
+            document.getElementById('p1').style.backgroundColor = 'orange';
+
+    }
+}
+function fun1() {
+    let a, b, c;
+    // document.getElementById('p1').style.backgroundColor='black';
+    for (a = 0; a < 1000; a++) {
+        if (a % 2 == 0)
+            document.getElementById('p1').style.backgroundColor = 'pink';
+        else
+            document.getElementById('p1').style.backgroundColor = 'red';
+
+    }
+}
